5ea785f12e472c6646abbec2827535d92b2d959d
Roman <dyndynroman@gmail.com>
2017-10-07 23:54:12 +0300

    lab2.2

---
+++ lab2.2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+import sys
+
+name = raw_input("What's your name? ")
+fathersName = raw_input("What's your father's name? ")
+
+print('Вітаю, {} {}'.format(name, fathersName))

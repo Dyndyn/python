d3d3d54159e6d0ed5da52fa42f9c3d51763bc959
Roman <dyndynroman@gmail.com>
2017-10-07 23:56:10 +0300

    lab2.2

--- lab2.2.py
+++ lab2.2.py
@@ -3,7 +3,8 @@
 
 import sys
 
+surname = raw_input("What's your surname? ")
 name = raw_input("What's your name? ")
 fathersName = raw_input("What's your father's name? ")
 
-print('Вітаю, {} {}'.format(name, fathersName))
+print('Вітаю, {} {} {}'.format(surname, name, fathersName))

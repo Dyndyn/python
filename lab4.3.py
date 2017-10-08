a76b2bcd9a840d916c9c84e71af0a531b77924ec
Roman <dyndynroman@gmail.com>
2017-10-08 16:31:55 +0300

    lab4.3

--- lab4.3.py
+++ lab4.3.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-import decimal
+from decimal import *
 
-decimal.getcontext().prec = 2
-salary = decimal.Decimal(input("What's your salary? "))
+#getcontext().prec = 2
+cents = Decimal('0.01')
+salary = Decimal(input("What's your salary? ")).quantize(cents, ROUND_HALF_UP)
 
-tax = salary * decimal.Decimal('0.18')
-military = salary * decimal.Decimal('0.015')
+tax = (salary * Decimal('0.18')).quantize(cents, ROUND_HALF_UP)
+military = (salary * Decimal('0.015')).quantize(cents, ROUND_HALF_UP)
 
 print('Податок на доходи фізичних осіб = %.2f, військовий збір = %.2f' % (tax, military))

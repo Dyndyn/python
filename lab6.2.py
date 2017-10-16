ad100c8909c4e8a4d4923be9abd4cea70eb53d40
Roman <dyndynroman@gmail.com>
2017-10-16 18:19:10 +0300

    lab6.2

---
+++ lab6.2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from decimal import * 
+cents = Decimal('0.01')
+
+summa = input("What's the summa? ")
+percentage = Decimal(input("What's the percentage (within a hundred)? ")/100.0)
+duration = input("What's the duration (in years)? ")
+result = Decimal(summa)
+i = 0
+while i < duration:
+     result = (result + result*percentage).quantize(cents, ROUND_HALF_UP)
+     i = i + 1
+
+print ("result = %.2f" % result)
+
+

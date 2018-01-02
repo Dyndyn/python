56cefc5b2c472e7d2679a413d08a7111c458ad30
Roman <dyndynroman@gmail.com>
2018-01-03 01:13:34 +0200

    lab8.2

---
+++ lab8.2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+
+def quicksort(array):
+    less = []
+    equal = []
+    greater = []
+
+    if len(array) > 1:
+        pivot = array[0]
+        for x in array:
+            if x < pivot:
+                less.append(x)
+            if x == pivot:
+                equal.append(x)
+            if x > pivot:
+                greater.append(x)
+        return quicksort(less) + equal + quicksort(greater)
+    else:
+        return array
+
+print(quicksort([12,4,5,6,7,3,1,15,7,18,20,2,16]))

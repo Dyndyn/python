17b8f597314efed5e5f541419b10b69e07e9d7e6
Roman <dyndynroman@gmail.com>
2018-01-02 21:59:51 +0200

    lab7.2

--- lab7.2.py
+++ lab7.2.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
-
+import re
 
 s = input("enter the string? ")
 
 def isPalindrome(string):
+    string = re.sub(r'\W+', '', string).lower()
     for i,char in enumerate(string):
         if char != string[-i-1]:
             return False
